a3d1d44c9a3b11973c3d80918643167b1483fb3f
Izann_Brum <61673451+Izann-Brum@users.noreply.github.com>
2022-06-21 21:40:04 -0300

    Delete Projeto directory

--- Projeto/database/database.ini.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-require 'connection.php';
-
-use ConexaoPHPPostgres\Connection as Connection;
-
-$pdo = Connection::get()->connect();
--- Projeto/database/models.php
+++
@@ -1,7 +0,0 @@
-<?php
-require 'models/Usuario.php';
-require 'models/Unidade.php';
-require 'models/Editora.php';
-require 'models/Livro.php';
-require 'models/Autor.php';
-require 'models/Emprestimo.php';
