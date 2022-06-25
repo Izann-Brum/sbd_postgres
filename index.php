c65f2f1805ea80bf61930d0a843b08917a6088b1
Izann <izannrogerio@gmail.com>
2022-06-25 15:09:14 -0300

    update

--- index.php
+++ index.php
@@ -17,7 +17,7 @@ include('templates/header.php');
         <div class="col-lg-6">
             <img src="./assets/images/Logo_UFT.png" width="156px" height="107px">
             <br><br>
-            <h1 class="display-4" style="font-weight:bold !important; font-size: 48px; font-family: 'Roboto', sans-serif; color: White;"> Sistema Bibliotecário Digital </h1>
+            <h1 class="display-4" style="font-weight:bold !important; font-size: 40px; font-family: 'Roboto', sans-serif; color: White;"> Sistema Bibliotecário Digital </h1>
             <p class="lead" style="font-size: 24px; font-family: 'Roboto', sans-serif; color: #FFFFFF;  opacity: 0.7; line-height:100%">Exemplo de CRUD conectando ao PostgresSQL usando PHP, de maneira simples a fim de ser objetiva e intuitiva.</p>
         </div>
     </div>
