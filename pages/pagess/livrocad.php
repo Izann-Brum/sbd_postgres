8722d050ea1e4618f5a1c9f5c8cc6561d4b99361
Izann <izannrogerio@gmail.com>
2022-06-25 19:21:56 -0300

    u

--- pages/pagess/livrocad.php
+++ pages/pagess/livrocad.php
@@ -84,6 +84,14 @@ include('../../templates/header.php');
                     <?php endforeach; ?>
                     </select></center></td>
                 </div>
+                <select name="Fruit">
+                <option value="" disabled selected>Choose option</option>
+                <option value="Apple">Apple</option>
+                <option value="Banana">Banana</option>
+                <option value="Coconut">Coconut</option>
+                <option value="Blueberry">Blueberry</option>
+                <option value="Strawberry">Strawberry</option>
+                </select>
 
                 <!-- BOTÃO CADASTRAR -->
                 <div class="u-align-center u-form-group u-form-submit">
