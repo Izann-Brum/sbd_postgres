36e46423fd868323d701c4aee0f6e199ae3e077b
Izann <izannrogerio@gmail.com>
2022-06-25 17:38:43 -0300

    update

--- pages/pagess/unidadecad.php
+++ pages/pagess/unidadecad.php
@@ -6,7 +6,7 @@ include('../../templates/header.php');
   <head>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta charset="utf-8">
-    <meta name="keywords" content="CADASTRO AUTOR">
+    <meta name="keywords" content="CADASTRO UNIDADE">
     <meta name="description" content="">
     <title>teste</title>
     <link rel="stylesheet" href="nicepage.css" media="screen">
@@ -31,14 +31,20 @@ include('../../templates/header.php');
       <div class="u-clearfix u-sheet u-sheet-1">
         <div class="u-container-style u-custom-color-4 u-group u-radius-9 u-shape-round u-group-1">
           <div class="u-container-layout u-container-layout-1">
-            <h2 class="u-text u-text-body-alt-color u-text-default u-text-1">CADASTRO AUTOR</h2>
+            <h2 class="u-text u-text-body-alt-color u-text-default u-text-1">CADASTRO UNIDADE</h2>
             <div class="u-form u-form-1">
-              <form action="../Controllers/Autor.php" method="POST" source="custom" name="form" style="padding: 10px;">
+              <form action="../Controllers/Unidade.php" method="POST" source="custom" name="form" style="padding: 10px;">
                 
                 <!-- CAMPO NOME -->
                 <div class="u-form-group u-form-name">
                   <label for="name-9b6e" class="u-form-control-hidden u-label"></label>
                   <input type="text" placeholder="Nome" id="name-9b6e" name="Nome" class="u-border-1 u-border-grey-30 u-input u-input-rectangle u-white" required="" maxlength="64">
+                </div>
+
+                <!-- CAMPO ENDEREÇO -->
+                <div class="u-form-group">
+                  <label for="email-9b6e" class="u-form-control-hidden u-label"></label>
+                  <input type="text" placeholder="Endereço" id="email-9b6e" name="Endereco" class="u-border-1 u-border-grey-30 u-input u-input-rectangle u-white" required="required" maxlength="64">
                 </div>
 
                 <!-- BOTÃO CADASTRAR -->
