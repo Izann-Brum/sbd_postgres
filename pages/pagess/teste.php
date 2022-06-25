19a70107338be22ce189430e8112cd65e60adee2
Izann <izannrogerio@gmail.com>
2022-06-25 15:28:53 -0300

    update

--- pages/pagess/teste.php
+++ pages/pagess/teste.php
@@ -30,37 +30,36 @@ include('../../templates/header.php');
   <body class="u-body u-overlap u-overlap-transparent u-xl-mode">
     <section class="u-align-center u-clearfix u-image u-section-1" id="sec-4cf6" data-image-width="6000" data-image-height="4000">
       <div class="u-clearfix u-sheet u-sheet-1">
-        <div class="u-custom-color-3 u-opacity u-opacity-95 u-radius-9 u-shape u-shape-round u-shape-1"></div>
-        <h2 class="u-text u-text-body-alt-color u-text-1">CADASTRO EDITORA</h2>
-        <div class="u-form u-form-1">
-          <form action="#" method="POST" class="u-clearfix u-form-spacing-10 u-form-vertical u-inner-form" source="custom" name="form" style="padding: 10px;">
-            
-            
-            <div class="u-form-group u-form-name">
-              <label for="name-9b6e" class="u-form-control-hidden u-label"></label>
-              <input type="text" placeholder="Nome" id="name-9b6e" name="name" class="u-border-1 u-border-grey-30 u-input u-input-rectangle u-white" required="">
+        <div class="u-container-style u-custom-color-4 u-group u-opacity u-opacity-95 u-radius-9 u-shape-round u-group-1">
+          <div class="u-container-layout u-container-layout-1">
+            <h2 class="u-text u-text-body-alt-color u-text-1">CADASTRO EDITORA</h2>
+            <div class="u-form u-form-1">
+              <form action="#" method="POST" class="u-clearfix u-form-spacing-10 u-form-vertical u-inner-form" source="custom" name="form" style="padding: 10px;">
+                <div class="u-form-group u-form-name">
+                  <label for="name-9b6e" class="u-form-control-hidden u-label"></label>
+                  <input type="text" placeholder="Nome" id="name-9b6e" name="name" class="u-border-1 u-border-grey-30 u-input u-input-rectangle u-white" required="">
+                </div>
+                <div class="u-form-email u-form-group">
+                  <label for="email-9b6e" class="u-form-control-hidden u-label"></label>
+                  <input type="email" placeholder="Endereço" id="email-9b6e" name="email" class="u-border-1 u-border-grey-30 u-input u-input-rectangle u-white" required="">
+                </div>
+                <div class="u-form-group u-form-group-3">
+                  <label for="text-9737" class="u-form-control-hidden u-label"></label>
+                  <input type="text" placeholder="Telefone" id="text-9737" name="text" class="u-border-1 u-border-grey-30 u-input u-input-rectangle u-white">
+                </div>
+                <div class="u-align-center u-form-group u-form-submit">
+                  <a href="#" class="u-border-2 u-border-grey-75 u-btn u-btn-submit u-button-style u-palette-3-dark-1 u-btn-1">Cadastrar</a>
+                  <input type="submit" value="submit" class="u-form-control-hidden">
+                </div>
+              </form>
             </div>
-            <div class="u-form-email u-form-group">
-              <label for="email-9b6e" class="u-form-control-hidden u-label"></label>
-              <input type="email" placeholder="Endereço" id="email-9b6e" name="email" class="u-border-1 u-border-grey-30 u-input u-input-rectangle u-white" required="">
-            </div>
-            <div class="u-form-group u-form-group-3">
-              <label for="text-9737" class="u-form-control-hidden u-label"></label>
-              <input type="text" placeholder="Telefone" id="text-9737" name="text" class="u-border-1 u-border-grey-30 u-input u-input-rectangle u-white">
-            </div>
-            <div class="u-align-center u-form-group u-form-submit">
-              <a href="#" class="u-btn u-btn-submit u-button-style">Cadastrar</a>
-              <input type="submit" value="submit" class="u-form-control-hidden">
-            </div>
-          
-          </form>
+          </div>
         </div>
       </div>
     </section>
+    
   </body>
 </html>
-
 <?php
 include('../../templates/footer.php');
 ?>
-
