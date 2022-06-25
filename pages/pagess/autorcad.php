c762774e52e7381131fd299677e66ba5d8f8a0da
Izann <izannrogerio@gmail.com>
2022-06-25 17:17:36 -0300

    UPDATE

--- pages/pagess/autorcad.php
+++ pages/pagess/autorcad.php
@@ -1,27 +1,6 @@
 <?php
 include('../../templates/header.php');
 ?>
-
-<!DOCTYPE html>
-<html style="font-size: 16px;">
-  <head>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta charset="utf-8">
-    <meta name="keywords" content="cADASTRO AUTOR">
-    <meta name="description" content="">
-    <!-- <title>autocad</title> -->
-    <link rel="stylesheet" href="nicepage.css" media="screen">
-<link rel="stylesheet" href="autorcad.css" media="screen">
-    <script class="u-script" type="text/javascript" src="jquery.js" defer=""></script>
-    <script class="u-script" type="text/javascript" src="nicepage.js" defer=""></script>
-    <meta name="generator" content="Nicepage 4.12.21, nicepage.com">
-    <link id="u-theme-google-font" rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i|Open+Sans:300,300i,400,400i,500,500i,600,600i,700,700i,800,800i">
-    
-    
-    <script type="application/ld+json">{
-		"@context": "http://schema.org",<?php
-include('../../templates/header.php');
-?>
 <!DOCTYPE html>
 <html style="font-size: 16px;">
   <head>
@@ -62,17 +41,6 @@ include('../../templates/header.php');
                   <input type="text" placeholder="Nome" id="name-9b6e" name="Nome" class="u-border-1 u-border-grey-30 u-input u-input-rectangle u-white" required="" maxlength="64">
                 </div>
 
-                <!-- CAMPO ENDEREÇO -->
-                <div class="u-form-group">
-                  <label for="email-9b6e" class="u-form-control-hidden u-label"></label>
-                  <input type="text" placeholder="Endereço" id="email-9b6e" name="Endereco" class="u-border-1 u-border-grey-30 u-input u-input-rectangle u-white" required="required" maxlength="64">
-                </div>
-
-                <!-- CAMPO TELEFONE -->
-                <div class="u-form-group u-form-phone u-form-group-3">
-                  <label for="text-9737" class="u-form-control-hidden u-label"></label>
-                  <input type="text" placeholder="Telefone" id="text-9737" name="Telefone" class="u-border-1 u-border-grey-30 u-input u-input-rectangle u-white"  maxlength="14" required="required">
-                </div>
 
                 <!-- BOTÃO CADASTRAR -->
                 <div class="u-align-center u-form-group u-form-submit">
@@ -92,42 +60,6 @@ include('../../templates/header.php');
 
   </body>
 </html>
-<?php
-include('../../templates/footer.php');
-?>
-		"@type": "Organization",
-		"name": ""
-}</script>
-    <meta name="theme-color" content="#478ac9">
-    <meta property="og:title" content="autocad">
-    <meta property="og:type" content="website">
-  </head>
-  <body class="u-body u-xl-mode">
-    <section class="u-clearfix u-image u-section-1" id="sec-4cf6" data-image-width="6000" data-image-height="4000">
-      <div class="u-clearfix u-sheet u-sheet-1">
-        <div class="u-custom-color-3 u-opacity u-opacity-90 u-radius-9 u-shape u-shape-round u-shape-1"></div>
-        <h2 class="u-text u-text-body-alt-color u-text-1">CADASTRO AUTOR</h2>
-        <div class="u-form u-form-1">
-          <form action="../Controllers/Autor.php" method="POST" style="padding: 18px;">
-           
-          <div class="u-form-group u-form-name u-label-none">
-              <!-- <label for="name-ef64" class="u-label">seila</label> -->
-              <input type="text" placeholder="Nome" id="name-ef64" name="Nome" class="u-border-1 u-border-grey-30 u-input u-input-rectangle u-white u-input-1" required="" autofocus="autofocus" maxlength="64">
-            </div>
-            <div class="u-form-group u-form-submit">
-              <a href="#" class="u-btn u-btn-submit u-button-style">Cadastrar</a>
-              <input type="submit" value="Cadastrar" name="submit" class="u-form-control-hidden">
-            </div>
-         
-          </form>
-        </div>
-      </div>
-    </section>
-    
-
-  </body>
-</html>
-
 <?php
 include('../../templates/footer.php');
 ?>
