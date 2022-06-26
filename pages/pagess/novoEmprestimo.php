016ee523938f45f2ecc8cf686c11f68f3b32ccfd
Izann <izannrogerio@gmail.com>
2022-06-25 21:02:09 -0300

    update

--- pages/pagess/novoEmprestimo.php
+++ pages/pagess/novoEmprestimo.php
@@ -23,10 +23,20 @@ try {
 
 ?>
 
-
 <?php
 include('../../templates/header.php');
 ?>
+
+<div id="bb1" style = "min-height: 100vh;">
+<?php
+if (isset($_GET['MSGERROR'])){
+	echo '<h2 style="color:red"><center>'.$_GET['MSGERROR'].'</h2></center>';
+}
+if (isset($_GET['MSG'])){
+	echo '<h2 style="color:green"><center>'.$_GET['MSG'].'</h2></center>';
+}
+?>
+
 <!DOCTYPE html>
 <html style="font-size: 16px;">
   <head>
