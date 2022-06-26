81f5c34eba383e0726125c39007aa08e8a282e8f
Izann <izannrogerio@gmail.com>
2022-06-26 18:20:11 -0300

    update

--- pages/Controllers/Editora.php
+++ pages/Controllers/Editora.php
@@ -35,7 +35,7 @@ if ($_POST['submit']=='Cadastrar'){
 
 			$nome_key = "EDITORA_Nome_key";
 
-			if (mb_strpos($sr, $nome_key) == true) {
+			if (strpos($sr, $nome_key) !== false) {
 				header("Location: ../../pages/CadEditora.php?MSGERROR=Nome de editora indisponível");
 			}else {
 				header("Location: ../../pages/CadEditora.php?MSGERROR=Erro não especificado");
