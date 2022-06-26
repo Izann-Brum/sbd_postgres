2b33587404cc6233b74068a0b243293968089145
Izann <izannrogerio@gmail.com>
2022-06-26 14:26:27 -0300

    update

--- pages/Controllers/Emprestimo.php
+++ pages/Controllers/Emprestimo.php
@@ -68,10 +68,10 @@ if ($_POST['submit']=='INSERIR'){
 				 echo $e->getMessage();
 				$sr = serialize($e);
 
-				$nome_key = "LIVRO_EMPRESTIMO_pk";
+				$nome_key = '"LIVRO_EMPRESTIMO_pk"';
 				echo('teste 1 ');
 
-				if (mb_strpos($sr, $nome_key) !== true) {
+				if (mb_strpos($sr, $nome_key) !== false) {
 					echo('teste 2 ');
 					header("Location: ../../pages/pagess/novoEmprestimo.php?MSGERROR=Usário já possuí título");
 				}else {
