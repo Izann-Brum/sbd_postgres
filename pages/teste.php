6f256e69e36c2e91974537860724d7cdc99a3d16
Izann <izannrogerio@gmail.com>
2022-06-26 14:40:24 -0300

    update

--- pages/teste.php
+++ pages/teste.php
@@ -19,6 +19,22 @@ try {
 }
 ?>
 
+<?php
+include('../templates/header.php');
+?>
+
+<div id="bb1" style = "min-height: 100vh;">
+<?php
+if (isset($_GET['MSGERROR'])){
+	echo '<h2 style="color:red"><center>'.$_GET['MSGERROR'].'</h2></center>';
+}
+if (isset($_GET['MSG'])){
+	echo '<h2 style="color:green"><center>'.$_GET['MSG'].'</h2></center>';
+}
+?>
+
+
+
 <style> table {background: #FFFFFF; box-shadow: 0px 16px 32px rgba(5, 18, 34, 0.08); border-radius: 24px;  font-family: arial, sans-serif;   border-collapse: collapse;   width: 100%; padding: 32px;}  
 td, th {   border: 1.5px solid #dddddd;   text-align: center;   padding: 8px; } 
 tr {   background-color: #ffffff; } </style>
@@ -43,24 +59,8 @@ tr {   background-color: #ffffff; } </style>
        
 </table>
 </div>
+<!-- --------------------------------------------------------------------------------------------------- -->
 
-<?php
-include('../templates/header.php');
-?>
-
-<div id="bb1" style = "min-height: 100vh;">
-<?php
-if (isset($_GET['MSGERROR'])){
-	echo '<h2 style="color:red"><center>'.$_GET['MSGERROR'].'</h2></center>';
-}
-if (isset($_GET['MSG'])){
-	echo '<h2 style="color:green"><center>'.$_GET['MSG'].'</h2></center>';
-}
-?>
-
-<style> table {background: #FFFFFF; box-shadow: 0px 16px 32px rgba(5, 18, 34, 0.08); border-radius: 24px;  font-family: arial, sans-serif;   border-collapse: collapse;   width: 100%; padding: 32px;}  
-td, th {   border: 1.5px solid #dddddd;   text-align: center;   padding: 8px; } 
-tr {   background-color: #ffffff; } </style>
 <table class="table">
         <tr>
         <th>Selecione o autor</th>
@@ -80,6 +80,7 @@ tr {   background-color: #ffffff; } </style>
        
 </table>
 </div>
+
 
 <?php
 include('../templates/footer.php');
