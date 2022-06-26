9d77cedc5909e42a849cf72a70f9a2bac6d86459
Izann <izannrogerio@gmail.com>
2022-06-26 14:31:07 -0300

    update

--- pages/Controllers/Emprestimo.php
+++ pages/Controllers/Emprestimo.php
@@ -71,7 +71,7 @@ if ($_POST['submit']=='INSERIR'){
 				$nome_key = '"LIVRO_EMPRESTIMO_pk"';
 				echo('teste 1 ');
 
-				if (mb_strpos($sr, $nome_key) !== false) {
+				if (strpos($sr, $nome_key) !== false) {
 					echo('teste 2 ');
 					header("Location: ../../pages/pagess/novoEmprestimo.php?MSGERROR=Usário já possuí título");
 				}else {
