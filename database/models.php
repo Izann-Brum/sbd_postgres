d5a635380dd5fd5a5401c6162ff9c935a25c741f
Izann_Brum <61673451+Izann-Brum@users.noreply.github.com>
2022-07-16 17:26:56 -0300

    Update models.php

--- database/models.php
+++ database/models.php
@@ -1,7 +1,9 @@
 <?php
+echo('models');
 require 'models/Usuario.php';
 require 'models/Unidade.php';
 require 'models/Editora.php';
 require 'models/Livro.php';
 require 'models/Autor.php';
 require 'models/Emprestimo.php';
+?>
